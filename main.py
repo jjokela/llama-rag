1a688778b9df822b46b9cafcf69e6da745c1bea2
CARDANOADS\j.jokela <j.jokela@cardano.com>
2023-12-11 17:29:59 +0100

    added initial code

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import os
+from dotenv import load_dotenv
+from llama_index import SimpleDirectoryReader, VectorStoreIndex
+
+load_dotenv()
+
+GCP_PROJECT_ID = os.getenv('GCP_PROJECT_ID')
+SERVICE_ACCOUNT_FILE = os.getenv('SERVICE_ACCOUNT_FILE')
+STORAGE_BUCKET_NAME = os.getenv('STORAGE_BUCKET_NAME')
+
+documents = SimpleDirectoryReader("./docs").load_data()
+index = VectorStoreIndex.from_documents(documents)
+
 def main():
     print("jou")
 
